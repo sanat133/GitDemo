f57f93fcc2ea371f46f6f739f102ef6141cf3b5e
sanat <tanishasergill@gmail.com>
2021-05-15 18:19:47 +0530

    first commit..

---
+++ src/com/acc/Test.java
@@ -0,0 +1,13 @@
+package com.acc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "sanat";
+		System.out.println(s);
+
+	}
+
+}
